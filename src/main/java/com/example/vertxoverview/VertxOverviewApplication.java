42d9efb7a186e96c75badb068e21ef8c2950b1e7
oldfr <ankitha.codes@gmail.com>
2023-03-18 17:39:40 +0530

    initial commit

---
+++ src/main/java/com/example/vertxoverview/VertxOverviewApplication.java
@@ -0,0 +1,14 @@
+package com.example.vertxoverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertxOverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertxOverviewApplication.class, args);
+	}
+
+
+}
